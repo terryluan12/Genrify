cc28859fde9831f61e16319fa1af29f2dda64f45
Terry <terryluan12@gmail.com>
2023-07-15 23:06:02 -0400

    Made genrify main files more readable/user friendly

--- src/genrify.py
+++ src/genrify.py
@@ -2,8 +2,11 @@ from datasources import download_datasets
 from preprocessing import preprocess
 import os
 
+# Change to subset used to train the CNN
+subset_num = -1
+
 
 if not os.path.isdir("datasources/Data") and not os.path.isdir("datasources/processed_data"):
     download_datasets()
 
-preprocess(0)
+preprocess(subset_num)
