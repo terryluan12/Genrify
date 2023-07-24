bfe02984aa66c0cbef84ff99f16bc88f649ece82
Terry <terryluan12@gmail.com>
2023-07-24 02:32:40 -0400

    Added save_mel_spectrogram_image and combined get_mel_spectrograms and extract_feature_mel_spectrogram

--- src/utils/mel_spectrogram.py
+++ src/utils/mel_spectrogram.py
@@ -12,28 +12,33 @@ def extract_features_mel_spectrogram(datapoint):
     Args:
         datapoint (tuple(numpy.ndarray, int)):  Tuple of librosa sampled audio file
     Returns:
-        mel_spec_db (np.array): Spectrogram features in dB (number of frequncy bins) x (number of time frames)
+        mel_spectrogram_db (np.array): Spectrogram features in dB (number of frequncy bins) x (number of time frames)
     """
     audio, sample_rate = datapoint
-    mel_spectrogram = librosa.stft(audio) 
-    mel_spec = librosa.feature.melspectrogram(mel_spectrogram)
-    mel_spec_db = librosa.amplitude_to_db(abs(mel_spec))
+    mel_spectrogram = librosa.feature.melspectrogram(y=audio, sr=sample_rate)
+    mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
 
-    return mel_spec_db
+    return mel_spectrogram_db
 
-def get_mel_spectrograms():
 
-  audio_file = '/content/file_example_WAV_1MG.wav'
-  audio, sample_rate = librosa.load(audio_file, sr=None)
-  mel_spectrogram = librosa.feature.melspectrogram(y=audio, sr=sample_rate)
-  mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
+def save_mel_spectrogram_image(mel_spectrogram_data, file_name):
+    """
+    Save mel-spectrogram data as an image
 
-  plt.figure(figsize=(10, 4))
-  librosa.display.specshow(mel_spectrogram_db, sr=sample_rate, x_axis='time', y_axis='mel')
-  plt.colorbar(format='%+2.0f dB')
-  plt.title('Mel-Spectrogram')
-  plt.show()
-
+    Args:
+        spectrogram_data (np.array): Spectrogram features in dB (number of frequncy bins) x (number of time frames) 
+        file_name (str): path to save image
+    """
+    if mel_spectrogram_data is None:
+        return
+    plt.figure(figsize=(10, 4), frameon=False)
+    plt.ylim(0, 11000) # Always plot up to 11kHz
+    librosa.display.specshow(mel_spectrogram_data, x_axis='time', y_axis='hz', vmin=-40, vmax=40) # Always show magnitude from -40 to +40 dB
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(file_name)
+    plt.cla()
+    plt.close('all')
 
 
 def convert_to_mel_spectrogram_images(datasets, root_dir="."):
@@ -54,6 +59,6 @@ def convert_to_mel_spectrogram_images(datasets, root_dir="."):
             os.makedirs(os.path.join(processed_data_dir, str(label)), exist_ok=True)
             mel_spectrogram_data = extract_features_mel_spectrogram(data)
             image_path = os.path.join(processed_data_dir, str(label), f"{i}.png")
-            save_spectrogram_image(mel_spectrogram_data, image_path)  
+            save_mel_spectrogram_image(mel_spectrogram_data, image_path)  
             
             i += 1
