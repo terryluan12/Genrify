a1dcd7032710cd75b04081a60487cb14b7d969e6
nicholasshort <nicholas.r.h.short@gmail.com>
2023-07-26 12:16:39 -0400

    Adding weight decay

--- src/cnn/training_spectrogram.py
+++ src/cnn/training_spectrogram.py
@@ -5,9 +5,11 @@ from torch.optim.lr_scheduler import StepLR
 from cnn.training import get_model_name
 import os
 
-def train(model, train_loader, val_loader, num_epochs, learning_rate, batch_size, step_size, gamma):
+def train(model, train_loader, val_loader, num_epochs, learning_rate, batch_size, step_size, gamma, weight_decay):
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Modify the optimizer initialization to include weight decay
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
