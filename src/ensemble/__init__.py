04eefb7db7f00d5ac3c34605076354ef4f18e37d
nicholasshort <nicholas.r.h.short@gmail.com>
2023-07-26 18:21:26 -0400

    Added get_models function

---
+++ src/ensemble/__init__.py
@@ -0,0 +1,3 @@
+from .ensemble import get_models
+
+spectrogram_model, _, _, _ = get_models()
--- src/ensemble/ensemble.py
+++ src/ensemble/ensemble.py
@@ -1,7 +1,25 @@
+from cnn import spectrogram_model, mfcc_model #chroma_model, mel_spectrogram_model
+import torch
 import os
 
-def load_cnns(root_dir="."):
-    final_model_dir = os.path.join(root_dir, "datasources", "final_models")
-    print(final_model_dir)
+def get_models(dir="/content/drive/MyDrive/APS360 Team Project/Final Models"):
+    spectrogram = spectrogram_model.Spectrogram_CNN()
+    # mfcc = mfcc_model.MFCC_CNN()
+    # chroma = chroma_model.Chroma_CNN()
+    # mel_spectrogram  = mel_spectrogram_model.Mel_Spectrogram_CNN()
+
+    spectrogram_state = torch.load(os.path.join(dir, "spectrogram_model.pt"))
+    # mfcc_state = torch.load(os.path.join(dir, "mfcc_model.pt"))
+    # chroma_state = torch.load(os.path.join(dir, "chroma_model.pt"))
+    # mel_spectrogram_state = torch.load(os.path.join(dir, "mel_spectrogram_model.pt"))
+
+    spectrogram.load_state_dict(spectrogram_state)
+    # mfcc.load_state_dict(mfcc_state)
+    # chroma.load_state_dict(chroma_state)
+    # mel_spectrogram.load_state_dict(mel_spectrogram_state)
+
+
+    return spectrogram, None, None, None
+    # return spectrogram, mfcc, chroma_model, mel_spectrogram
 
 
