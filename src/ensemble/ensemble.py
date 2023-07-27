63d1c8429a54142e05146e4531def4f8853832e5
nicholasshort <nicholas.r.h.short@gmail.com>
2023-07-27 00:48:18 -0400

    Forgot to save

--- src/ensemble/ensemble.py
+++ src/ensemble/ensemble.py
@@ -28,7 +28,6 @@ def full_model(data_loader, cuda=True, weak_learners=None):
 
     device = torch.device("cuda" if (torch.cuda.is_available() and cuda) else "cpu")
 
-    # Move all weak learners to the device
     for model in weak_learners:
         model.to(device)
         model.eval()
@@ -36,12 +35,11 @@ def full_model(data_loader, cuda=True, weak_learners=None):
     correct = 0
     total = 0
 
-    with torch.no_grad():  # Disable gradient tracking during inference
+    with torch.no_grad():
         for inputs, labels in data_loader:
             inputs = inputs.to(device)
             labels = labels.to(device)
 
-            # Inference for each weak learner
             all_predictions = []
             for model in weak_learners:
                 outputs = model(inputs)
@@ -50,8 +48,7 @@ def full_model(data_loader, cuda=True, weak_learners=None):
 
             stacked_predictions = torch.stack(all_predictions, dim=1)
 
-            # Perform majority voting to get final predictions
-            majority_vote, _ = torch.mode(stacked_predictions, dim=1)
+            majority_vote, _ = torch.mode(stacked_predictions, dim=0)
 
             correct += (majority_vote == labels).sum().item()
             total += labels.size(0)
