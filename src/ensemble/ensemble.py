439e4ff2462f94258fec5542ab6f8f3558351230
nicholasshort <nicholas.r.h.short@gmail.com>
2023-08-01 17:13:30 -0400

    Uncommenting loading other models

--- src/ensemble/ensemble.py
+++ src/ensemble/ensemble.py
@@ -9,17 +9,16 @@ def get_weak_learners(dir="/content/drive/MyDrive/APS360 Team Project/Final Mode
     mel_spectrogram  = mel_model.MEL_CNN()
 
     spectrogram_state = torch.load(os.path.join(dir, "spectrogram_model.pt"))
-    # mfcc_state = torch.load(os.path.join(dir, "mfcc_model.pt"))
+    mfcc_state = torch.load(os.path.join(dir, "mfcc_model.pt"))
     chroma_state = torch.load(os.path.join(dir, "chroma_model.pt"))
-    # mel_spectrogram_state = torch.load(os.path.join(dir, "mel_spectrogram_model.pt"))
+    mel_spectrogram_state = torch.load(os.path.join(dir, "mel_spectrogram_model.pt"))
 
     spectrogram.load_state_dict(spectrogram_state)
-    # mfcc.load_state_dict(mfcc_state)
+    mfcc.load_state_dict(mfcc_state)
     chroma.load_state_dict(chroma_state)
-    # mel_spectrogram.load_state_dict(mel_spectrogram_state)
+    mel_spectrogram.load_state_dict(mel_spectrogram_state)
 
-    return spectrogram, spectrogram, spectrogram, spectrogram
-    # return spectrogram, mfcc, chroma, mel_spectrogram
+    return spectrogram, mfcc, chroma, mel_spectrogram
 
 def full_model(test_loaders, cuda=True, weak_learners=None):
     if weak_learners is None:
